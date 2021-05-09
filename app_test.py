2e27ae69349e9bf1a3f464a12e9ecbc6719c8655
Georgios Moschovis <geomos@kth.se>
2021-05-10 00:09:40 +0300

    New driver including data and targets.

--- app_test.py
+++ app_test.py
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from datasets.data_loaders import TimitDataset
+from datasets.data_loaders_2 import TimitDataset
 from datasets.data_transformations import MFCC
+from datasets.data_transformations import Phonemes
 from datasets.corpus import *
-from datasets.phonemes import *
 
 FLAGS = flags.FLAGS
 
@@ -32,25 +32,30 @@ def main(argv):
     targets = len(phonemes)  # Number of categories
 
     # Load the TIMIT dataset
-    dataset = TimitDataset(csv_file='test_data.csv',
+    dataset = TimitDataset(csv_file = 'test_data.csv',
                            root_dir = dataset_dir,
-                           transform=MFCC(n_fft=FLAGS.n_fft,
+                           corpus = corpus,
+                           transform = MFCC(n_fft=FLAGS.n_fft,
                                           preemphasis_coefficient=FLAGS.preemphasis_coefficient,
-                                          num_ceps=FLAGS.num_ceps))
+                                          num_ceps=FLAGS.num_ceps),
+                           transcription = Phonemes(n_fft=FLAGS.n_fft,
+                                           preemphasis_coefficient=FLAGS.preemphasis_coefficient,
+                                           num_ceps=FLAGS.num_ceps,
+                                           corpus = corpus))
 
+    ele = dataset[1]
+#    print(ele['phonemes_per_frame'])
     # Get the MFCC coefficients
-    train_data = getMFCCFeatures(dataset)
+    train_data, max_len = getMFCCFeatures(dataset)
+    
+    # Get the phonemes per frame (as percentages)
+    train_targets = getTargetPhonemes(dataset, max_len, corpus, train_data)
 
-    # Initialize a Phonemes object
-    audio_files_phonemes = Phonemes(corpus, dataset_dir)
     
-    # Get the phonemes information
-    train_phonemes, test_phonemes = getPhonemesInformation(audio_files_phonemes)
-    
-def getMFCCFeatures(dataset):
+def getMFCCFeatures(dataset, zeropad = False):
     """ This method computes the MFCC coefficients per frame.
          When frames are less than the maximum amount does zero-padding.
-         @returns arrays of MFCC coefficients of the same length
+         @returns tensors of MFCC coefficients of the same length
     """
     features = []
     tensors = []
@@ -63,48 +68,68 @@ def getMFCCFeatures(dataset):
                 max_frames = audio.shape[0]
             features.append(audio)
     
-    for i in range(len(dataset)):
+    if(zeropad == True):
+        # zero-padding for equal length
+        for i in range(len(dataset)):
             audio_new = np.zeros((max_frames, features[i].shape[1]))
             audio_new[0:features[i].shape[0],:] = features[i]
             tensors.append(torch.tensor(audio_new.tolist(), dtype=torch.long))
-    
-    return tensors
+    else:
+        for i in range(len(dataset)):
+            audio_new = features[i]
+            tensors.append(torch.tensor(audio_new.tolist(), dtype=torch.long))
 
-def  getPhonemesInformation(audio_files_phonemes):
-    """ This method loads the phonemes information.
-        It considers audio file in the training and test set.
-        @returns train_phonemes, test_phonemes
-    
-                train_phonemes is a list of the following elements:
-                train_phonemes[i][0] = the directory of the i^th audio file
-                train_phonemes[i][1] = a list of the times every phoneme starts in the i^th audio file
-                train_phonemes[i][2] = a list of the times every phoneme ends in the i^th audio file
-                train_phonemes[i][3] = a list of transcriptions of all the phonemes in the i^th audio file
-                train_phonemes[i][4] = a list of numerical IDs of all the phonemes in the i^th audio file
-                train_phonemes[i][5] = a list of 1-hot-vector representation of the numerical IDs of all the phonemes in the i^th audio file
-                
-                test_phonemes has a similar structure
+    return tensors, max_frames
+
+def getTargetPhonemes(dataset, max_frames, corpus, mfccs, zeropad = False, onehot = True):
+    """ This method computes the target phonemes as percentages per frame.
+         @returns tensors of phonemes per frame
     """
-    train_phonemes = audio_files_phonemes.getTrainElements()
-    
-    # Get the phonemes information for every audio file in the test set
-    test_phonemes = audio_files_phonemes.getTestElements()
+    tensors = []
+    targets = []
+    max_frames = -1
 
-    return train_phonemes, test_phonemes
-    
-def test1(dataset):
     for i in range(len(dataset)):
-            sample = dataset[i]
+        sample = dataset[i]
+        phoneme_list = sample['phonemes_per_frame']
+        sample_targets = []
+        for j in range(len(phoneme_list)):
+            if(onehot == False):
+                if(len(phoneme_list[j]) == 1):
+                    # only one phoneme in this frame --> one hot encoding
+                    the_one_phoneme = phoneme_list[j][0][2]
+                    the_one_phoneme_one_hot = corpus.phones_to_onehot([the_one_phoneme])[0]
+                    sample_targets.append(the_one_phoneme_one_hot)
+                else:
+                    # more than one phonemes in this frame --> probabilities
+                    complex_one_hot = np.zeros(len(corpus.get_phonemes())).tolist()
+                    for k in range(len(phoneme_list[j])):
+                        the_kth_phoneme = phoneme_list[j][k][2]
+                        the_kth_phoneme_ID = corpus.get_phones_ID(the_kth_phoneme)
+                        the_kth_phoneme_percentage = phoneme_list[j][k][3]
+                        complex_one_hot[the_kth_phoneme_ID] = the_kth_phoneme_percentage
+                        sample_targets.append(complex_one_hot)
+            else:
+                # using only one phoneme explicitly imposed --> one hot encoding
+                the_one_phoneme = phoneme_list[j][0][2]
+                the_one_phoneme_one_hot = corpus.phones_to_onehot([the_one_phoneme])[0]
+                sample_targets.append(the_one_phoneme_one_hot)
+        targets.append(sample_targets)
+    
+    if(zeropad == True):
+        # silence-padding for equal length (like zero-padding for the MFCCs - don't know if works)
+        for i in range(len(dataset)):
+            sample_targets = targets[i]
+            for j in range(len(sample_targets), max_frames, 1):
+                # adding a silence target for the extra frames (zero-padded additions)
+                silence = corpus.get_silence()
+                silence_one_hot = corpus.phones_to_onehot([silence])[0]
+                targets[i].append(silence_one_hot)
+        
+    for i in range(len(targets)):
+        tensors.append(torch.tensor(targets[i], dtype=torch.long))
 
-            audio = np.asarray(sample['audio'])
-
-            print("Audio shape: ", audio.shape)
-
-            plt.pcolormesh(audio)
-            plt.show()
-
-            if i == 1:
-                break
+    return tensors
 
 if __name__ == '__main__':
     flags.DEFINE_integer('n_fft', 512, 'Size of FFT')
